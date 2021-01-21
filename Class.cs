429b392bf0156aacf7f71df54a32c08b4d6056ce
AndreasEliasson91 <77665227+AndreasEliasson91@users.noreply.github.com>
2021-01-21 14:35:10 +0100

    Wrote Classes + Adjust Logics & Flow

--- Class.cs
+++ Class.cs
@@ -2,36 +2,108 @@ using System;
 
 namespace Task09
 {
-    class Game
+    class Players
     {
         public string[] selections = new string[3] { "ROCK", "PAPER", "SCISSOR"};
-        public string move;
+        public string move, name;
         public int num;
+        public Random randNum = new Random();
     }
-    class Player : Game
+    class Human : Players
     {
-        Player()
+        public Human()
         {
-            move = Move();
+            name = SetName();
+            Console.WriteLine("PLAYER NAME: " + name);
+        }
+        public string SetName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            name = name.ToUpper();
+            return name;
         }
         public string Move()
         {
-            Console.WriteLine("Type 1 for ROCK, 2 for PAPER or 3 for SCISSOR: ");
-            num = Convert.ToInt32(Console.ReadLine());
+            bool loop;
+            do
+            {
+                loop = false;
+                Console.WriteLine("Type 0 for ROCK, 1 for PAPER or 2 for SCISSOR: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > 3) 
+                {
+                    Console.WriteLine("Invalid entry, try again!");
+                    loop = true;
+                }
+            } while (loop == true);
             return selections[num];
         }
     }
-    class Computer : Game
+    class Computer : Players
     {
-        Computer()
+        public Computer()
         {
-            move = Move();
+            name = SetName();
+            Console.WriteLine("COMPUTER NAME: " + name);
+        }
+        public string SetName()
+        {
+            num = randNum.Next(3);
+            if (num == 0)   { name = "DONALD"; }
+            else if (num == 1)  { name = "RICHARD"; }
+            else    { name = "JOHN"; }
+            return name;
         }
         public string Move()
         {
-            Random randNum = new Random();
             num = randNum.Next(3);
             return selections[num];
+        }
+    }
+    class ScoreBoard
+    {
+        public int[] score;
+
+        public ScoreBoard()
+        {
+            score = new int[2] { 0, 0 };
+        }
+        public void DisplayScore(ScoreBoard scoreboard, Human player, Computer comp) { Console.WriteLine(player.name + ": " + scoreboard.score[0] + "\n" + comp.name + ": " + scoreboard.score[1] + "\n"); }
+        public int[] GetScore(ScoreBoard scoreboard, Human player, Computer comp)
+        {  
+            if (player.move == comp.move) { Console.WriteLine("\nIt's a TIE, no points!\n"); }
+            else if (player.move == "ROCK" && comp.move == "PAPER")
+            {
+                Console.WriteLine("\nPoint to " + comp.name + "!\n");
+                scoreboard.score[1]++;
+            }
+            else if (player.move == "ROCK" && comp.move == "SCISSOR")
+            {
+                Console.WriteLine("\nPoint to " + player.name + "!\n");
+                scoreboard.score[0]++;
+            }
+            else if (player.move == "SCISSOR" && comp.move == "PAPER")
+            {
+                Console.WriteLine("\nPoint to " + player.name + "!\n");
+                scoreboard.score[0]++;
+            }
+            else if (player.move == "SCISSOR" && comp.move == "ROCK")
+            {
+                Console.WriteLine("\nPoint to " + comp.name + "!\n");
+                scoreboard.score[1]++;
+            }
+            else if (player.move == "PAPER" && comp.move == "ROCK")
+            {
+                Console.WriteLine("\nPoint to " + player.name + "!\n");
+                scoreboard.score[0]++;
+            }
+            else if (player.move == "PAPER" && comp.move == "SCISSOR")
+            {
+                Console.WriteLine("\nPoint to " + comp.name + "!\n");
+                scoreboard.score[1]++;
+            }
+            return scoreboard.score;
         }
     }
 }
--- Main.cs
+++ Main.cs
@@ -6,83 +6,21 @@ namespace Task09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Rock, Paper or Scissor game against the computer. First to 3 wins!\n\n");
-            int[] score = new int[2] {0,0};
-            string playerMove, computerMove;
-            while (score[0] < 3 && score[1] < 3)
+            Console.WriteLine("This is a simple *Rock, Paper or Scissor-Game* against the computer. First to 3 wins!\n\n");
+            ScoreBoard scoreboard01 = new ScoreBoard();
+            Human player01 = new Human();
+            Computer computer01 = new Computer();
+            while (scoreboard01.score[0] < 3 && scoreboard01.score[1] < 3)
             {
-                playerMove = PlayerMove();
-                computerMove = ComputerMove();
-                score = GetScore(score, playerMove, computerMove);
-                Console.WriteLine(score[0] + " - " + score[1] + "\n");
+                player01.move = player01.Move();
+                computer01.move = computer01.Move();
+                Console.WriteLine("\n" + player01.name + ": " + player01.move + "\n" + computer01.name + ": " + computer01.move);
+                scoreboard01.score = scoreboard01.GetScore(scoreboard01, player01, computer01);
+                scoreboard01.DisplayScore(scoreboard01, player01, computer01);
             }
-            if (score[0] > score[1]) { Console.WriteLine("PLAYER WIN!"); }
-            else { Console.WriteLine("COMPUTER WIN!"); }
+            if (scoreboard01.score[0] > scoreboard01.score[1]) { Console.WriteLine(player01.name + " WIN!"); }
+            else { Console.WriteLine(computer01.name + " WIN!"); }
             Console.ReadKey(); 
         }
-        static string PlayerMove()
-        {
-            bool loop;
-            string move;
-            do
-            {
-                loop = false;
-                Console.WriteLine("Rock, Paper or Scissor?");
-                move = Console.ReadLine();
-                move = move.ToUpper();
-                if (move == "ROCK" || move == "PAPER" || move == "SCISSOR") { continue; }
-                else
-                {
-                    Console.WriteLine("Invalid entry! Try again.\n");
-                    loop = true;
-                }
-            } while (loop == true);
-            Console.WriteLine("PLAYER: " + move);
-            return move;
-        }
-        static string ComputerMove()
-        {
-            Random randNum = new Random();
-            int num = randNum.Next(3);
-            string[] move = new string[3] { "ROCK", "PAPER", "SCISSOR" };
-            Console.WriteLine("COMPUTER: " + move[num]);
-            return move[num];
-        }
-        static int[] GetScore(int[] currentScore, string player, string computer)
-        {  
-            if (player == computer) { Console.WriteLine("It's a Tie!"); }
-            else if (player == "ROCK" && computer == "PAPER")
-            {
-                Console.WriteLine("Point to computer!");
-                currentScore[1]++;
-            }
-            else if (player == "ROCK" && computer == "SCISSOR")
-            {
-                Console.WriteLine("Point to player!");
-                currentScore[0]++;
-            }
-            else if (player == "SCISSOR" && computer == "PAPER")
-            {
-                Console.WriteLine("Point to player!");
-                currentScore[0]++;
-            }
-            else if (player == "SCISSOR" && computer == "ROCK")
-            {
-                Console.WriteLine("Point to computer!");
-                currentScore[1]++;
-            }
-            else if (player == "PAPER" && computer == "ROCK")
-            {
-                Console.WriteLine("Point to player!");
-                currentScore[0]++;
-            }
-            else if (player == "PAPER" && computer == "SCISSOR")
-            {
-                Console.WriteLine("Point to computer!");
-                currentScore[1]++;
-            }
-            return currentScore;
-        }
-    
     }
 }
